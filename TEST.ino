b43821a72d9ba3c09587a0c5d4cbf2d16da4af74
WheHeoHu <wheheohu@outlook.com>
2021-04-14 19:28:32 +0800

    add git ignore

--- TEST.ino
+++ TEST.ino
@@ -1,3 +1,6 @@
+
+#include <M5StickC.h>
+
 #include <BlueMagic32.h>
 
 #define BUTTON_PIN 0
@@ -10,9 +13,19 @@ unsigned long debounceDelay = 50;   // the debounce time; increase if the output
 
 void setup()
 {
+    M5.begin();
+    M5.IMU.Init();
 
-    Serial.begin(115200);
+    M5.Lcd.setRotation(1);
 
+    M5.Lcd.fillScreen(TFT_GREEN);
+
+    M5.Lcd.setTextColor(TFT_BLACK); // Adding a background colour erases previous text automatically
+
+    M5.Lcd.setCursor(10, 30);
+    M5.Lcd.setTextSize(3);
+    M5.Lcd.print("SHAKE ME");
+    
     BMDConnection.begin("AA");
     BMDControl = BMDConnection.connect();
 
@@ -22,29 +35,29 @@ void setup()
 void loop()
 {
 
-    int reading = digitalRead(BUTTON_PIN);
+    // int reading = digitalRead(BUTTON_PIN);
 
-    // If the switch changed, due to noise or pressing:
-    if (reading != lastButtonState)
-    {
-        lastDebounceTime = millis();
-    }
+    // // If the switch changed, due to noise or pressing:
+    // if (reading != lastButtonState)
+    // {
+    //     lastDebounceTime = millis();
+    // }
 
-    if ((millis() - lastDebounceTime) > debounceDelay)
-    {
-        if (reading != buttonState)
-        {
-            buttonState = reading;
+    // if ((millis() - lastDebounceTime) > debounceDelay)
+    // {
+    //     if (reading != buttonState)
+    //     {
+    //         buttonState = reading;
 
-            // only toggle the LED if the new button state is HIGH
-            if (buttonState == HIGH)
-            {
-                if (BMDConnection.available())
-                {
-                    BMDControl->toggleRecording();
-                }
-            }
-        }
-    }
-    lastButtonState = reading;
+    //         // only toggle the LED if the new button state is HIGH
+    //         if (buttonState == HIGH)
+    //         {
+    //             if (BMDConnection.available())
+    //             {
+    //                 BMDControl->toggleRecording();
+    //             }
+    //         }
+    //     }
+    // }
+    // lastButtonState = reading;
 }
