1739862671b423bd58d950457c152b42a66af5d0
WheHeoHu <wheheohu@outlook.com>
2021-04-17 20:40:43 +0800

    change pin number

--- BlueUnreal.ino
+++ BlueUnreal.ino
@@ -3,11 +3,11 @@
 
 #include <BlueMagic32.h>
 
-#define BUTTON_PIN 0
+#define BUTTON_PIN 37
 
 int buttonState;           // the current reading from the input pin
 int lastButtonState = LOW; // the previous reading from the input pin
-int loopcount = 0;
+
 
 unsigned long lastDebounceTime = 0; // the last time the output pin was toggled
 unsigned long debounceDelay = 50;   // the debounce time; increase if the output flickers
@@ -24,50 +24,51 @@ void setup()
     M5.Lcd.setCursor(10, 30);
     M5.Lcd.setTextSize(2);
     M5.Lcd.print("TRY CONNECT");
-
+   // BMDConnection.clearPairing();
     BMDConnection.begin("AA");
 
-   BMDControl = BMDConnection.connect();
+    BLEAddress pccadd = BLEAddress("90:FD:9F:C4:5F:C2");
+    BMDControl = BMDConnection.connect(pccadd);
 
+    M5.Lcd.setCursor(10, 30);
     M5.Lcd.fillScreen(BLACK);
-    M5.Lcd.print("TEST");
-
+    M5.Lcd.print("Connected");
+    delay(1000);
     pinMode(BUTTON_PIN, INPUT);
 }
 
 void loop()
 {
-    
-    M5.Lcd.setCursor(10, 30);
-    M5.Lcd.setTextSize(3);
-    M5.Lcd.fillScreen(BLACK);
-    M5.Lcd.print(loopcount);
-    Serial.println("TEST Serial");
-    loopcount++;
-    delay(1000);
-    // int reading = digitalRead(BUTTON_PIN);
 
-    // // If the switch changed, due to noise or pressing:
-    // if (reading != lastButtonState)
-    // {
-    //     lastDebounceTime = millis();
-    // }
+   
+    //Serial.println("TEST Serial");
 
-    // if ((millis() - lastDebounceTime) > debounceDelay)
-    // {
-    //     if (reading != buttonState)
-    //     {
-    //         buttonState = reading;
 
-    //         // only toggle the LED if the new button state is HIGH
-    //         if (buttonState == HIGH)
-    //         {
-    //             if (BMDConnection.available())
-    //             {
-    //                 BMDControl->toggleRecording();
-    //             }
-    //         }
-    //     }
-    // }
-    // lastButtonState = reading;
+    int reading = digitalRead(BUTTON_PIN);
+
+    // If the switch changed, due to noise or pressing:
+    if (reading != lastButtonState)
+    {
+        lastDebounceTime = millis();
+        Serial.println(lastDebounceTime);
+    }
+
+    if ((millis() - lastDebounceTime) > debounceDelay)
+    {
+        if (reading != buttonState)
+        {
+            buttonState = reading;
+
+            // only toggle the LED if the new button state is HIGH
+            if (buttonState == HIGH)
+            {
+                Serial.println("Rec");
+                if (BMDConnection.available())
+                {
+                    BMDControl->toggleRecording();
+                }
+            }
+        }
+    }
+    lastButtonState = reading;
 }
