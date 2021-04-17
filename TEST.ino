9931fa024fbead933be03a29bb5babb3f19ab95b
WheHeoHu <wheheohu@outlook.com>
2021-04-17 14:15:37 +0800

    serial issure

--- TEST.ino
+++ TEST.ino
@@ -7,34 +7,50 @@
 
 int buttonState;           // the current reading from the input pin
 int lastButtonState = LOW; // the previous reading from the input pin
+int loopcount = 0;
 
 unsigned long lastDebounceTime = 0; // the last time the output pin was toggled
 unsigned long debounceDelay = 50;   // the debounce time; increase if the output flickers
 
 void setup()
 {
+    Serial.begin(115200);
     M5.begin();
     M5.IMU.Init();
 
     M5.Lcd.setRotation(1);
 
-    M5.Lcd.fillScreen(TFT_GREEN);
-
-    M5.Lcd.setTextColor(TFT_BLACK); // Adding a background colour erases previous text automatically
+    M5.Lcd.setTextColor(TFT_WHITE); // Adding a background colour erases previous text automatically
 
     M5.Lcd.setCursor(10, 30);
-    M5.Lcd.setTextSize(3);
-    M5.Lcd.print("SHAKE ME");
-    
+    M5.Lcd.setTextSize(2);
+    M5.Lcd.print("TRY CONNECT");
+
     BMDConnection.begin("AA");
-    BMDControl = BMDConnection.connect();
+
+    //DEBUG AA
+    M5.Lcd.setCursor(10, 30);
+    M5.Lcd.fillScreen(BLACK);
+    M5.Lcd.print("Begin AA");
+
+    // BMDControl = BMDConnection.connect();
+
+    M5.Lcd.fillScreen(BLACK);
+    M5.Lcd.print("TEST");
 
     pinMode(BUTTON_PIN, INPUT);
 }
 
 void loop()
 {
-
+    
+    M5.Lcd.setCursor(10, 30);
+    M5.Lcd.setTextSize(3);
+    M5.Lcd.fillScreen(BLACK);
+    M5.Lcd.print(loopcount);
+    Serial.println("TEST Serial");
+    loopcount++;
+    delay(1000);
     // int reading = digitalRead(BUTTON_PIN);
 
     // // If the switch changed, due to noise or pressing:
