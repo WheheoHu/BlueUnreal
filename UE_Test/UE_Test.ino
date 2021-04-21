19b17c2ab2641d14a7124b30b16d837fa8769364
WheHeoHu <wheheohu@outlook.com>
2021-04-21 21:27:22 +0800

    delete

--- UE_Test/UE_Test.ino
+++ UE_Test/UE_Test.ino
@@ -1,7 +1,7 @@
 
 #include <M5StickC.h>
 #define LED_PIN 10
-#define BUTTON_PIN 37
+
 
 int buttonread = 0;
 void setup()
@@ -16,32 +16,49 @@ void setup()
     M5.Lcd.setCursor(10, 30);
     M5.Lcd.setTextSize(2);
     M5.Lcd.print("Boot");
-
-    pinMode(BUTTON_PIN, INPUT);
+    Serial.setTimeout(5);
     pinMode(LED_PIN, OUTPUT);
 }
 
 void loop()
 {
-    buttonread = digitalRead(BUTTON_PIN);
+
     // if (!Serial.available())
     //     return;
     // String str = Serial.readString();
 
-    if (buttonread)
-    {
-        digitalWrite(LED_PIN, 1);
-    }
-    else
-    {
-        digitalWrite(LED_PIN, 0);
+    // if (buttonread)
+    // {
+    //     digitalWrite(LED_PIN, 1);
+    // }
+    // else
+    // {
+    //     digitalWrite(LED_PIN, 0);
 
-        // int len=12;
-        // Serial.println(len);
-        float fVal = 12.34f;
-        byte *fBuffer = reinterpret_cast<byte *>(&fVal);
-        Serial.write(fBuffer, 4);
-
-        delay(100);
+    //     // int len=12;
+    //     // Serial.println(len);
+    //     float fVal = 12.34f;
+    //     byte *fBuffer = reinterpret_cast<byte *>(&fVal);
+    //     Serial.write(fBuffer, 4);
+    // }
+    if (Serial.available())
+    {
+        String str = Serial.readString();
+        if (str = "lines")
+        {
+            float fVal = 12.34f;
+            byte *fBuffer = reinterpret_cast<byte *>(&fVal);
+            Serial.write(fBuffer, 4);
+            digitalWrite(LED_PIN,HIGH);
+            M5.Lcd.fillScreen(TFT_BLACK);
+            M5.Lcd.setCursor(10, 30);
+            M5.Lcd.setTextSize(2);
+            M5.Lcd.print("Lines");
+        }
+        else
+        {
+            /* code */
+        }
+        
     }
 }
